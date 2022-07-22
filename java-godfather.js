b92eb34e397ef4883d24157fa9912fcafe2d3a2a
【яιѕнαвн-ρɾαωαรɦ-Malviya】ʟɛɢɛռɖ-Tɦɑղ❍ꜱ <109682273+LEGENDXTHANOS@users.noreply.github.com>
2022-07-22 10:10:31 -0700

    Create java-godfather.js

---
+++ java-godfather.js
@@ -0,0 +1,40 @@
+from utils.clints
+await: from notrun.pro
+                   int("legend-GOD")
+                   int("run")
+                   await: retune,
+                           retune await if not pro.user
+                           from userbot.files(.py).utils
+                           mirror_mod=x
+                           mirror_mod=y
+                           mirror_off==(java,python,hack,userbot,html)
+                           mirror_on=y==x
+                           print(await).nobe
+                           utils.root.userbot=github.com/Godfatherakkii/Godfather_Userbot.git
+                           RUN clone userbot files.
+                           if not pro.py 
+                           RUN= int(GODbot)srt("mirror_mod").git
+                           Run= int("mirror_off")str(GODBot_Pro)Runner.coder
+                           Run=X
+                           await : send_message(09675476.send).delete
+                              retune:delete(09675476.retune.send_message.sleep_time.py
+                           await : SUDO_USER= 1761766352,send_message.delete:sleep_time=10mhz
+                              retune : SUDO_USER , hacking.users_date=TEL_EGRAM-IDS=Clints_await=LEGEND-LX/PYTHONUSERBOT.git
+                           await =delete.send=target account hack,RUNPRO,
+                           """MAKA BHOSDA TUMHARI CHUDA AAB DEPLOY KARKE USERBOT BAAP KA MAL HE KIYA MADRCHODO """
+                           """ ___GODBOT__________________USERBOT=V5.0 COMING SOON...........AWAIT WAIT ALL..."""
+                           """________________________________DEVELOPER_BY= LEGENDHACKER FROM LEGEND-LX::::::::::"""
+                           await print(e)...........................................................................
+                              slep =10mhz
+                              root time=7:20pm
+                              run time= 10:20am
+                              off time :12:00=NIGHT=8.089.09.76\?
+                              on fire telegram=pro.py
+                              hacking basd=None,pro,python,hack,javascript,Go,shell,Clints.RUN
+                              """______________________________________________________________"""
+                              """______________________________________________________________"""
+                              """_-_-__----____----______-_-____-_-_____--_--_-___--_-_--_--____"""
+                              """____-_-____---_-__-_____----___---___---____--__--_--/-()9)()()()90()()87)()"""
+                              await.CLONE.TELEGRAM.
+                              #Clone #my_userbot_ PYTHONUSERBOT/LEGEND-LX.get
+                              #RUN
